521875242d2e1446ccb763dc2b5a3b70a73a7463
meisReal <simas.cesnakauskas@gmail.com>
2022-10-26 14:14:15 +0300

    Simple routes to serve html pages

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'project', 'index.html'));
+});
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'project', 'about.html'))
+});
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'project', 'contact-me.html'))
+});
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'project', '404.html'))
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
